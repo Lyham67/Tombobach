f09067613ea8be2ab1aeb03b23cd8e899e4eebd4
Bachelor&International <lyham.donnadieu@ensam.eu>
2025-10-22 21:12:26 +0200

    Create api-config.js

---
+++ api-config.js
@@ -0,0 +1,5 @@
+// Configuration de l'API backend
+// Remplace cette URL par ton URL Railway après déploiement
+const API_URL = 'tombobach-production.up.railway.app' ;
+
+// Exemple : const API_URL = 'https://tombobach-production.up.railway.app';
